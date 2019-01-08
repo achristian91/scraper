32bd14c0fbf219f5dd564478f2e29cf5c6e3b6c9
Arthur Christian <32134423+achristian91@users.noreply.github.com>
2019-01-07 22:10:32 -0500

    setup server

---
+++ server.js
@@ -0,0 +1,46 @@
+// Requires dependencies
+var express = require("express");
+var expressHandlebars = require("express-handlebars")
+var bodyParser = require("body-parser")
+var mongoose = require("mongoose")
+
+// Set up ports 
+var PORT = process.env.PORT || 3000;
+
+// Starts express app
+var app = express();
+
+// Creates express router  
+var router = express.Router();
+
+// Makes requests go through middleware
+app.use(express.static(__dirname + "/public"));
+
+//connects handlebars to express
+app.engine("handlebars", expressHandlebars({
+    defaultLayout: "main"
+}));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(router);
+
+//uses deployed db if not uses loacl mongoheadline 
+var db = process.env.MONGODB_URI || "mongodb//localhost/mongoHeadlines";
+
+//connects mongoose to database
+mongoose.connect(db, function(error) {
+    //Logs any errors
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log("mongoose connection is sucessful");
+    }
+});
+
+app.listen(PORT, function() {
+    console.log("Listening on port:" + PORT);
+});
